0daf8bfac9a87bfe40603d385172c39fd4ca2962
deepak thagunna <deepak.thagunna@swifttech.com>
2022-03-22 18:05:45 +0545

    1st

---
+++ index.php
@@ -0,0 +1,94 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Home (Hotel Management)</title>
+		<link rel="stylesheet" type="text/css" href="Css/Style.css">
+		<link  href="Img/hotel.png" type="img/icon"  rel="icon">
+	</head>
+	<body>
+	<div id="full">
+		<div style="background-image: url('Img/hotel-1.jpg'); width: 100%; height: 600px; ">
+			<div id="header">
+
+				<div id="logo">
+					<h1><font color="white">My Project</font></h1>
+
+					</div>
+						<div id="nav">
+							<ul>
+								<li><a href="#">Home </a></li>
+								<li><a href="#">Contact Us </a></li>
+								<li><a href="#">Book My Stay </a></li>
+								<li><a href="#">Our Hotels </a></li>
+								<li><a href="#">Help </a></li>
+
+							</ul>
+						</div>
+			
+					</div>
+			<div id="banner"></div>
+		</div>
+	<div id="f1">
+		<center><table>
+			<tr>
+				<th width="20%" height="50px"> Destination </th>
+				<th width="20%" height="50px"> Check In Date </th>
+				<th width="20%" height="50px"> Check Out Date </th>
+				<th width="20%" height="50px"> Room </th>
+				<td rowspan="2"><input type="submit" value="check" name="sub"></td>
+			</tr>
+			<tr>
+				<th width="20%" height="50px"> <input type="text" name="d1" placeholder="Enter Destination" name=""></th>
+				<th width="20%" height="50px"> <input type="date" name="ci" > </th>
+				<th width="20%" height="50px"> <input type="date" name="co"> </th>
+				<th width="20%" height="50px"> 
+					<select>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+
+					</select>
+				</th>
+			</tr>
+		</table>
+		</center>
+
+	</div>
+	<div id="welcome">
+			<h1 align="center">Welcome TO My Project</h1>
+		<center>
+				<font size="4"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </br>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </br> when an unknown printer took a galley of type and scrambled it to make a type specimen book.</br> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</br> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </br>and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</font>
+			<h2 style="color: red;">Our Brands</h2>
+		</center>
+	</div>
+	<div id="g1">
+		<div id="one"><img src="Img/hotel-2.jpg" width="100%">
+			<center>
+				<h2>Taj</h2>
+				<font>Hotel  And Restaurant</font>
+				<p>Taj Hotels is a chain of luxury hotels and a subsidiary of the Indian Hotels Company Limited, headquartered at Express Towers, Nariman Point, Mumbai. Incorporated by the founder of the Tata Group, Jamsetji Tata, in 1903, the company is a part of the Tata Group, one of India's largest business conglomerates</p><font color="red">More</font>
+			</center>
+		</div>
+		<div id="two"><img src="Img/hotel-3.jpg" width="100%">
+			<center>
+				<h2>Annapurna</h2>
+				<font>Hotel  And Restaurant</font>
+				<p>Taj Hotels is a chain of luxury hotels and a subsidiary of the Indian Hotels Company Limited, headquartered at Express Towers, Nariman Point, Mumbai. Incorporated by the founder of the Tata Group, Jamsetji Tata, in 1903, the company is a part of the Tata Group, one of India's largest business conglomerates</p><font color="red">More</font>
+			</center>
+		</div>
+		<div id="three"><img src="Img/hotel-4.jpg" width="100%">
+			<center>
+				<h2>Devotee</h2>
+				<font color="green">Hotel  And Restaurant</font>
+				<p><font color="red">Book Hotel Annapurna, Kathmandu on Tripadvisor: See 712 traveler reviews, 431 candid photos, and great deals for Hotel Annapurna, ranked #99 of 561 hotels ...Room features: Soundproof roomsAir conditio...Property amenities: Free parkingFree internet...Room types: Non-smoking roomsSuitesFamil</font></p><font color="red">More</font>
+
+			</center>
+		</div>
+	</div>
+	</body>
+	</html>
---
+++ r1.php
@@ -0,0 +1,94 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Home (Hotel Management)</title>
+		<link rel="stylesheet" type="text/css" href="Css/Style.css">
+		<link  href="Img/hotel.png" type="img/icon"  rel="icon">
+	</head>
+	<body>
+	<div id="full">
+		<div id="bg" style="background-image: url('Img/r1.jpg'); height: 1200px;">
+
+			<div id="header">
+
+				<div id="logo">
+					<h1><font color="white">My Project</font></h1>
+
+					</div>
+						<div id="nav">
+							<ul>
+								<li><a href="#">Home </a></li>
+								<li><a href="#">Contact Us </a></li>
+								<li><a href="#">Book My Stay </a></li>
+								<li><a href="#">Our Hotels </a></li>
+								<li><a href="#">Help </a></li>
+
+							</ul>
+						</div>
+			
+					</div>
+			<div id="banner">
+				<div id="form">
+				<table style="color: yellow;">
+						<tr>
+							<td>Status</td>
+							<td><input type="text" name="Status" placeholder="Status" title="Status"></td>						
+						</tr>
+							
+							<tr>
+								<td>Name</td>
+								<td><input type="text0" name="name" placeholder="Enter Name" title="Name"></td>
+								<td>ID No.</td>
+								<td><input type="text" name="indo" placeholder="Enter ID" title="ID"></td>
+							</tr>
+								<tr>
+									<td>Address</td>
+										<td><input type="text" name="address" placeholder="Enter Address" title="Address"> </td>
+								</tr>
+										<tr>
+											<td>City</td>
+											<td><select>
+												<option>--Select--</option>
+												<option>Mumbai</option>
+												<option>Delhi</option>
+												<option>Bengrulu</option>
+											</select></td>
+										</tr>
+									<tr>
+										<td>District</td>
+											<td><select>
+												<option>--Select--</option>
+												<option>Maharashtra</option>
+												<option>Delhi</option>
+												<option>Ap</option>
+										</select></td>
+									</tr>
+									<tr>
+										<td>Enter Your Mail</td>
+										<<td><input type="text" name="email" placeholder="Enter Email" title="E-mail"></td>
+									</tr>
+									<tr>
+										<td>Check-IN Date</td>
+										<td><input type="text" name="seein" placeholder="Check IN" title="Check"></td>
+										<td>Check Out</td>
+										<td><input type="text" name="seeout" placeholder="Check Out" title="Check Out"></td>
+									</tr>
+									<tr>
+										<td>Enter Member</td>
+										<td><input type="text" name="Member" placeholder="Enter Member" title="Member"></td>
+									</tr>
+									<td>
+										<td><input style="width: 120px; height: 30px; border-radius: 20px; opacity: 0.47;" type="submit" name="submit" value="submit"></td>
+									</td>
+
+						
+				</table>
+			</div>
+			</div>
+		</div>
+	
+	</body>
+	</html>
